9e7066e8a6e0eafc906cf2dace4ba665cc0a15c5
Daphine Kamusiime <daphnekamusiime09@gmail.com>
2024-03-12 11:20:03 +0300

    spring boot initialiser

---
+++ src/main/java/com/cicd/bscs/BscsApplication.java
@@ -0,0 +1,13 @@
+package com.cicd.bscs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BscsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BscsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cicd/bscs/BscsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cicd.bscs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BscsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
